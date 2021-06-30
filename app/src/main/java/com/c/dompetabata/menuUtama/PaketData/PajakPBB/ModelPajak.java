b75efe5742b6963ca19d87ee073996a32be8375a
beni hero <herobenih@gmail.com>
2021-06-30 09:25:42 +0700

    31-06-2021

--- app/src/main/java/com/c/dompetabata/menuUtama/PaketData/PajakPBB/ModelPajak.java
+++ app/src/main/java/com/c/dompetabata/menuUtama/PaketData/PajakPBB/ModelPajak.java
@@ -1,4 +1,14 @@
 package com.c.dompetabata.menuUtama.PaketData.PajakPBB;
 
 public class ModelPajak {
+
+    String id,code;
+
+    public String getId() {
+        return id;
+    }
+
+    public String getCode() {
+        return code;
+    }
 }
